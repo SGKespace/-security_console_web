0825093fc654f070af607eafd382276886d288be
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-25 18:12:31 +0400

    Исправил ошибки, почистил

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,35 +1,45 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.utils import timezone
 
 
-def get_duration(visit):  # время нахождения в хранилище
+def get_duration(visit):
     if not visit.leaved_at:
         return (timezone.localtime() - visit.entered_at).total_seconds()
     return (visit.leaved_at - visit.entered_at).total_seconds()
 
 
-def format_duration(time_delta):  # Делаем нужный для вывода формат
+def format_duration(time_delta):
     return '{:02}:{:02}:{:02}'.format(int(time_delta // 3600),
                                       int(time_delta % 3600 // 60),
                                       int(time_delta % 60),
                                       )
 
 
-def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+def is_visit_long(visit, minutes=60):
+    return get_duration(visit) // 60 > minutes
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+
+def passcard_info_view(request, passcode):
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    passcard_visits = get_list_or_404(Visit, passcard=passcard)
+
+    this_passcard_visits = []
+    for passcard_visit in passcard_visits:
+        time_delta = get_duration(passcard_visit)
+
+        visit = {
+            'entered_at': passcard_visit.entered_at,
+            'duration': format_duration(time_delta),
+            'is_strange': is_visit_long(passcard_visit),
+                 }
+
+        this_passcard_visits.append(visit)
+
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
-    }
+               }
+
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,26 +1,23 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils import timezone
+
+from datacenter.models import Visit
 from .passcard_info_view import format_duration, get_duration
 
 
 def storage_information_view(request):
-    # Программируем здесь
     visits = Visit.objects.filter(leaved_at__isnull=True)
-
     non_closed_visits = []
-
     for visit in visits:
         visit_element = {
             'who_entered': visit.passcard,
             'entered_at': visit.entered_at,
             'duration': format_duration(get_duration(visit)),
-        }
+                        }
         non_closed_visits.append(visit_element)
-
 
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
-    }
+              }
 
     return render(request, 'storage_information.html', context)
