f5ee85976314f4f9c81d2aa0efa6f3d105a8a562
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-27 10:01:54 +0400

    убрал каммент

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -17,7 +17,7 @@ def storage_information_view(request):
         non_closed_visits.append(visit_element)
 
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
               }
 
     return render(request, 'storage_information.html', context)
